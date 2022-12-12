211c3b6b50ed1ef41862c4f5dfb88f01bdd49047
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 10:49:39 -0600

    JS challenge involving using .push() method.

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const dirts of clay) {
+   const mug = `${dirts} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
